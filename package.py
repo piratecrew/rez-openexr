ade06721e2f467f17fb344d2f8274ed65c8a563c
Fredrik Brännbacka <fredrik.brannbacka@ilpvfx.com>
2021-11-10 09:56:30 +0100

    add devtoolset as build_requires

--- package.py
+++ package.py
@@ -2,6 +2,16 @@ name = "openexr"
 
 version = "3.1.3"
 
+@early()
+def build_requires():
+    # check if the system gcc is too old <9
+    # then we require devtoolset-9
+    from subprocess import check_output
+    valid = check_output(r"expr `gcc -dumpversion | cut -f1 -d.` \>= 9 || true", shell=True).strip().decode() == "1"
+    if not valid:
+        return ["devtoolset-9"]
+    return []
+
 build_command = "make -f {root}/Makefile {install}"
 
 def commands():
